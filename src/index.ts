7b59e0a17056f19729d94de8e0ce99c2b22e9cde
Your Name <you@example.com>
2025-04-09 04:30:41 +0000

    add WALLET_CONNECT, SIGN_MESSAGE, SIGN_TRANSFER_TRANSACTION actions

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import type { Plugin } from "@elizaos/core";
 
-import transfer from "./action/transfer";
-
-export { transfer };
+import TransferAction from "./action/transfer.ts";
+import WalletConnectAction from "./action/wallet_connect.ts";
+import SignMessageAction from "./action/sign_message.ts";
+import SignTransferTransactionAction from "./action/sign_transfer_transaction.ts";
+import GearApiService from "./service/GearApi.ts";
+import SignClientService from "./service/SignClient.ts";
 
 export const varaPlugin: Plugin = {
   name: "vara",
   description: "Vara integration plugin",
   providers: [],
   evaluators: [],
-  services: [],
-  actions: [transfer],
+  services: [
+    GearApiService,
+    SignClientService,
+  ],
+  actions: [
+    TransferAction,
+    WalletConnectAction,
+    SignMessageAction,
+    SignTransferTransactionAction,
+  ],
 };
 
 export default varaPlugin;
